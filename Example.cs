25f03118cab7c5fff5430cad4556dce682d2cdef
Knarik-Dabaghyan <69726910+Knarik-Dabaghyan@users.noreply.github.com>
2020-08-16 17:25:38 +0400

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+			 static void Test()
+		 {
+		  Console.WriteLine("Hello World!!!!: Test");
+		    Console.ReadLine();
+		  }
+    }
+}
